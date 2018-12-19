53ff09840ac6e253467fb9b6c5c7469c39a6320a
Jérôme Fink <jerome.fink@student.unamur.be>
2018-12-19 11:25:28 +0100

    Solving spelling mistake

--- src/main.py
+++ src/main.py
@@ -6,11 +6,14 @@ start_move = np.zeros((8, 8))
 end_move = np.zeros((8, 8))
 
 start_move[1, 1] = 1
-end_move[1, 2] = 1
+end_move[2, 1] = 1
 
 move = (start_move, end_move)
 
 chess_game = ChessGame()
+
+print(chess_game.board)
+print(start_move)
 
 # Print la taille du vecteur qui permettra d'encoder les mouvements (taille de l'output de notre réseau)
 print(chess_game.moves_shape)
